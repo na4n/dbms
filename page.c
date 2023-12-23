e54c4db55d49ac2c256fd40d42495151872a3d7a
Nathan Annareddy <nathan.annareddy@gmail.com>
2023-12-23 12:55:23 -0600

    implement delete

--- page.c
+++ page.c
@@ -9,19 +9,19 @@ const int PAGE_SIZE = 4096;
 struct page_header{
   int tupct;
   float ver;
-  int nfree;
+  int nfreedat;
+  int nfreetup;
 } typedef phead;
 
 struct tuple_header{
-  char fmt[10];
-  int loc;
-  int tsize;
+  unsigned char size;
+  short loc;
 } typedef thead;
 
-phead PAGE_HEAD_INITIALIZER = {.tupct=-1, .ver=-1, .nfree=-1};
+phead PAGE_HEAD_INITIALIZER = {.tupct=-1, .ver=-1, .nfreedat=-1, .nfreetup=-1};
 
 int phead_null(phead p){
-  return p.tupct == -1 && p.ver == -1 && p.nfree == -1;
+  return p.tupct == -1 && p.ver == -1 && p.nfreedat == -1 && p.nfreetup == -1;
 }
 
 phead phead_ret(char *pname){
@@ -62,7 +62,7 @@ int page_init(char *pname){
   }
   fclose(fp);
 
-  phead p = {.tupct=0, .ver=VERSION, .nfree=PAGE_SIZE};
+  phead p = {.tupct=0, .ver=VERSION, .nfreedat=PAGE_SIZE, .nfreetup=(int)sizeof(phead)};
   return phead_wrt(pname, &p);
 }
 
@@ -96,21 +96,21 @@ int tuple_add(char *pname, char *fmt, void **arg) {
   if(phead_null(p)){
     return 1;
   }
+
+  int curr_thead_loc = p.nfreetup;
+
   p.tupct += 1;
-  p.nfree -= l;
+  p.nfreedat -= l;
+  p.nfreetup += sizeof(thead);
 
-  int nexthead_end = sizeof(phead) + ((p.tupct) * (sizeof(thead)));
-  int nexttuple_end = p.nfree;
-
-  if(nexthead_end >= nexttuple_end){
-    printf("terminating condition is met\n");
+  if(p.nfreetup >= p.nfreedat){
+    printf("page is full\n");
     return 2;
   }
 
-  printf("adding %s ", fmt);
-  printf("with size %d\n", l);
+  // printf("adding %s ", fmt);
+  // printf("with size %d\n", l);
 
-  //write new head
   if(phead_wrt(pname, &p) == 1){
     return 1;
   }
@@ -120,23 +120,22 @@ int tuple_add(char *pname, char *fmt, void **arg) {
   if((fp = fopen(pname, "r+")) == NULL){
     return 1;
   }
-
-  thead t = {.fmt="", .loc=p.nfree, .tsize=l};
-  strcpy(t.fmt, fmt);
-  fseek(fp, nexthead_end-sizeof(thead), SEEK_SET);
-  fwrite(&t, sizeof(thead), 1, fp);
-
-  //write tuple at offset
-  fseek(fp, p.nfree, SEEK_SET);
-  fwrite(buf, l, 1, fp);
-
-  fclose(fp);
   
+  thead t = {.loc=p.nfreedat, .size=l};
+
+  fseek(fp, curr_thead_loc, SEEK_SET);
+  fwrite(&t, sizeof(thead), 1, fp);
+  
+  //write tuple at offset
+  fseek(fp, p.nfreedat, SEEK_SET);
+  fwrite(buf, l, 1, fp);
+  
+  fclose(fp);
   free(buf);
   return 0;
 }
 
-int tuple_decode(char *pname, int t){
+int tuple_decode(char *pname, int t, char *fmt){
   phead p = phead_ret(pname);
   if(phead_null(p)){
     return 1; 
@@ -156,27 +155,27 @@ int tuple_decode(char *pname, int t){
 
   thead th;
   fread(&th, sizeof(thead), 1, fp);
-  printf("format: %s\nlocation: %d\ntsize: %d\n\n", th.fmt, th.loc, th.tsize);
+  printf("tuple size: %d\ntuple location: %d\n\n", th.size, th.loc);
 
-  char buf[th.tsize];
+  char buf[th.size];
   fseek(fp, th.loc, SEEK_SET);
-  fread(buf, th.tsize, 1, fp);
+  fread(buf, th.size, 1, fp);
 
   int bufloc = 0;
-  for(int i = 0; i < strlen(th.fmt); i++){
-    if(th.fmt[i] == 'l'){
+  for(int i = 0; i < strlen(fmt); i++){
+    if(fmt[i] == 'l'){
       long longch;
       memcpy(&longch, buf+bufloc, sizeof(long));
       bufloc += sizeof(long);
-      printf("found a long in %s: %ld\n", th.fmt, longch);
+      printf("found a long in %s: %ld\n", fmt, longch);
     }
-    else if(th.fmt[i] == 'f'){
+    else if(fmt[i] == 'f'){
       double floatch;
       memcpy(&floatch, buf+bufloc, sizeof(double));
       bufloc += sizeof(double);
-      printf("found a double in %s: %f\n", th.fmt, floatch);
+      printf("found a double in %s: %f\n", fmt, floatch);
     }
-    else if(th.fmt[i] == 's'){
+    else if(fmt[i] == 's'){
       char *strng = NULL;
       int j = 0;
       while(1){
@@ -194,7 +193,7 @@ int tuple_decode(char *pname, int t){
           break;
         }
       }
-      printf("found a string in %s: %s\n", th.fmt, strng);
+      printf("found a string in %s: %s\n", fmt, strng);
       free(strng);
       continue;
     }
@@ -204,47 +203,53 @@ int tuple_decode(char *pname, int t){
 }
 
 int tuple_remove(char *pname, int n){
-  FILE *fp = fopen(pname, "r");
+  FILE *fp = fopen(pname, "r+");
   if(fp == NULL){
     return 1;
   }
 
   phead p = phead_ret(pname);
-  printf("file has %d tuples\n", p.tupct);
   if(n > p.tupct || n <= 0){
     fclose(fp);
+    printf("invalid tuple number\n");
     return 1;
   }
+
+  int th_loc = sizeof(phead)+(n-1)*sizeof(thead);
+  thead th;
+  fseek(fp, th_loc, SEEK_SET);
+  fread(&th, sizeof(thead), 1, fp);
   
-  thead t;
-  fseek(fp, (n-1)*sizeof(thead)+sizeof(phead), SEEK_SET);
-  fread(&t, 1, sizeof(thead), fp);
-
-  printf("format: %s\nloc: %d\nsize: %d\n", t.fmt, t.loc, t.tsize); //FP is not at 0 at this point
-
-  // thead lt;
-  // if(n < p.tupct){
-  //   fseek(fp, (p.tupct-1)*sizeof(thead)+sizeof(phead), SEEK_SET);
-  //   fread(&lt, 1, sizeof(thead), fp);
-  // }
-  if(n == p.tupct){ //just delete
-    fclose(fp);
-    return 1;
+  int t_loc = th.loc;
+  fseek(fp, t_loc, SEEK_SET);
+  char b = '\0';
+  long s;
+  
+  for(int i = 0; i < th.size; i++){
+    s = fwrite(&b, 1, 1, fp);
+    if(s == 0){
+      printf("something is going wrong\n");
+    }
   }
-  else if(n <= p.tupct){ //delete then move tuples and data 
-    fclose(fp);
-    return 1;
+  fseek(fp, th_loc, SEEK_SET);
+  for(int i = 0; i < sizeof(thead); i++){
+    s = fwrite(&b, 1, 1, fp);
+    if(s == 0){
+      printf("something is going wrong\n");
+    }
   }
-
-
   fclose(fp);
-  return 1;
+
+  FILE *garbage = fopen(".garbage", "a+");
+  fwrite(&n, sizeof(int), 1, garbage);
+  fclose(garbage);
+
+  return 0;
 }
 
 int main(int argc, char **argv){
   if(argc == 1){  //DEFAULT TEST CASE
-    //tuple_remove("test", 1);
-    tuple_decode("test", 1);
+    tuple_remove("test", 1);
     return 0;
   }
 
@@ -283,13 +288,16 @@ int main(int argc, char **argv){
       free(buf[i]);
     }
     free(buf);
+
+    tuple_decode("test", 1, argfrmt);
+    tuple_decode("test", 2, argfrmt);
   }
   else if(strcmp(argv[1], "decode") == 0){
     if(!(argc >= 4 && (atoi(argv[3]) != 0 || strcmp(argv[3], "0") == 0))){
       printf("usage: ./a.out decode [page name] [tuple]");
       return 1;
     }
-    tuple_decode(argv[2], atoi(argv[3]));
+    tuple_decode(argv[2], atoi(argv[3]), "quick");
   }
 
   return 0;
